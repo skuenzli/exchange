1911cacae215397fdbd140e540dc3a2ff057375f
Stephen Kuenzli <skuenzli@gmail.com>
2011-09-03 14:53:01 -0700

    Update description in project.clj.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject exchange "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Exchange is a simple stock exchange built for the purpose of learning Clojure."
   :dependencies [[org.clojure/clojure "1.2.1"]])
